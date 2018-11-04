1d59949de9ab76d1485ed0cc3e4ff4dd2db653e9
tyamanaka777 <tomohisa0513777@gmail.com>
2018-11-04 21:52:51 +0900

    高木さんがアップしたPlayerController.csの編集 ItohBallにスクリプトをアタッチし、実装

--- Tamakorogashi/Assets/Script/PlayerController.cs
+++ Tamakorogashi/Assets/Script/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed = 20;    //動く速さ
+
+    private Rigidbody rb; //Rigidbody
+
 	// Use this for initialization
 	void Start () {
-		
+        //Rigidbody取得
+        rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        //カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        //Rigidbodyに力を与えて玉を動かす
+        rb.AddForce(movement * speed);
 	}
 }
